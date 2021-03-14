f908db2575071e0f01b264281c71b8bbd0e1807b
ezginaztekin <80275365+ezginaztekin@users.noreply.github.com>
2021-03-14 18:10:25 +0300

    Update HW2.py

--- Homeworks/HW2.py
+++ Homeworks/HW2.py
@@ -1,5 +1,11 @@
-#Explain your work
+cv1={"name": "Ezgi Naz","surname": "Tekin","age" : "25","job":"Master student", "school": "ODTÜ" }
+cv2={"name": "Ahmet","surname": "Yılmaz","age" : "28","job":"Civil Engineer", "school": "Boğaziçi University" }
+cv3={"name": "Berna","surname": "Işık","age" : "22","job":"student", "school": "İTÜ" }
+cv4={"name": "Elif","surname": "Gültekin","age" : "24","job":"Architect", "school": "Bilkent University" }
+cv5={"name": "Ali","surname": "Bayram","age" : "23","job":"Computer Engineer", "school": "Bahçeşehir University" }
 
-#Question 1
-for x in range(a):
-	print(a)
+print(cv1)
+print(cv2)
+print(cv3)
+print(cv4)
+print(cv5)
